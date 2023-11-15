3abc45c7adefb254c402f4e5a9bd8d21ef00995e
Gonzalo <gonzalo.rubio.gutierrez@gmail.com>
2023-11-15 11:02:11 +0100

    Correción con chatgpt porque todo explotaba

--- app/src/main/java/com/example/sumadora/MainActivity.kt
+++ app/src/main/java/com/example/sumadora/MainActivity.kt
@@ -3,18 +3,152 @@ package com.example.sumadora
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.core.view.WindowCompat
-import com.example.sumadora.ui.theme.SumadoraTheme
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
-            SumadoraTheme {
-                SumadoraScreen()
-            }
+            SumadoraApp()
         }
     }
 }
 
+@Composable
+fun SumadoraApp() {
+   val viewModel: SumadoraScreen = viewModel()
+
+    var currentScreen by remember { mutableStateOf<Pantalla>(Pantalla.Input) }
+
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            when (currentScreen) {
+                is Pantalla.Input -> {
+                    InputScreen(
+                        viewModel = viewModel,
+                        onSumResult = {
+                            currentScreen = Pantalla.Result(it)
+                        }
+                    )
+                }
+                is Pantalla.Result -> {
+                    val resultado = currentScreen as Pantalla.Result
+                    Resultados(viewModel, resultado.result) {
+
+                        currentScreen = Pantalla.Input
+                    }
+                }
+            }
+
+
+        }
+    }
+}
+
+
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun InputScreen(viewModel: SumadoraScreen, onSumResult: (String) -> Unit) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(MaterialTheme.colorScheme.surface)
+            .padding(16.dp)
+    ) {
+        OutlinedTextField(
+            value = viewModel.numero1,
+            onValueChange = { viewModel.numero1 = it },
+            label = { Text("Número 1") },
+            keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+        )
+
+        OutlinedTextField(
+            value = viewModel.numero2,
+            onValueChange = { viewModel.numero2 = it },
+            label = { Text("Número 2") },
+            keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+        )
+
+        Button(
+            onClick = {
+                viewModel.sumar()
+                onSumResult(viewModel.resultadoActual)
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+        ) {
+            Text("Sumar")
+        }
+    }
+}
+
+
+@Composable
+fun Resultados(viewModel: SumadoraScreen, result: String, onBack: () -> Unit) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(MaterialTheme.colorScheme.surface)
+            .padding(20.dp)
+    ) {
+        Text("Resultado actual:", style = MaterialTheme.typography.labelMedium)
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(result, style = MaterialTheme.typography.bodySmall)
+        Spacer(modifier = Modifier.height(16.dp))
+        Text("Resultados anteriores:", style = MaterialTheme.typography.labelMedium)
+        Spacer(modifier = Modifier.height(8.dp))
+
+
+        val resultadosAnterioresText = buildString {
+            for (resultado in viewModel.resultadosAnteriores) {
+                append(resultado)
+                append("\n")
+            }
+        }
+
+        Text(resultadosAnterioresText, style = MaterialTheme.typography.bodySmall)
+
+        // Botón para volver a la pantalla de entrada
+        Button(
+            onClick = onBack,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+        ) {
+            Text("Volver a la entrada")
+        }
+    }
+}
+
+
+sealed class Pantalla {
+    object Input : Pantalla()
+    data class Result(val result: String) : Pantalla()
+}
--- app/src/main/java/com/example/sumadora/SumadoraScreen.kt
+++ app/src/main/java/com/example/sumadora/SumadoraScreen.kt
@@ -1,109 +1,36 @@
 package com.example.sumadora
-
-import android.os.Bundle
-import androidx.annotation.StringRes
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.widthIn
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+// SumadoraViewModel.kt
 import androidx.compose.runtime.getValue
+import androidx.lifecycle.ViewModel
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.semantics.Role.Companion.Button
-import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
-import androidx.navigation.NavHost
-import androidx.compose.foundation.layout.Column as Column
 
-enum class SumadoraScreen (@StringRes val title: Int){
-    Start(title = R.string.app_name),
-    Realizados(title = R.string.realizados)
-}
+class SumadoraScreen : ViewModel() {
+    // Estado para los números de las cajas de texto
+    var numero1 by mutableStateOf("")
+    var numero2 by mutableStateOf("")
 
-@Composable
-fun SumadoraScreen(
-    valor1: String,
-    valor2: String,
-    onSumButtonClick: (String, String) -> Unit,
-    navController: NavController,
-    modifier: Modifier = Modifier
-) {
-    Surface(
-        modifier = modifier,
-        color = MaterialTheme.colorScheme.background
-    ) {
-        Column(
-            modifier = modifier,
-            verticalArrangement = Arrangement.SpaceBetween
-        ) {
-            Column(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_small))
-            ) {
-                Spacer(modifier = Modifier.height(dimensionResource(R.dimen.padding_medium)))
-                Text(
-                    text = stringResource(R.string.valor1),
-                    style = MaterialTheme.typography.headlineSmall
-                )
-                Spacer(modifier = Modifier.height(dimensionResource(R.dimen.padding_small)))
-                EditNumberField(
-                    value = valor1,
-                    onValueChange = { /* Handle value change if needed */ },
-                    modifier = Modifier
-                        .padding(bottom = 32.dp)
-                        .fillMaxWidth()
-                )
-                Spacer(modifier = Modifier.height(dimensionResource(R.dimen.padding_medium)))
-                Text(
-                    text = stringResource(R.string.valor2),
-                    style = MaterialTheme.typography.headlineSmall
-                )
-                Spacer(modifier = Modifier.height(dimensionResource(R.dimen.padding_small)))
-                EditNumberField(
-                    value = valor2,
-                    onValueChange = { /* Handle value change if needed */ },
-                    modifier = Modifier
-                        .padding(bottom = 32.dp)
-                        .fillMaxWidth()
-                )
-            }
-            Row(modifier = Modifier.weight(1f, false)) {
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.spacedBy(
-                        dimensionResource(id = R.dimen.padding_medium)
-                    )
-                ) {
-                    // Botón de suma
-                    Button(
-                        onClick = {
-                            onSumButtonClick(valor1, valor2)
-                            // Navegar a la pantalla de resultados
-                            navController.navigate("resultados")
-                        },
-                        modifier = Modifier
-                            .widthIn(min = 250.dp)
-                            .padding(top = 16.dp)
-                    ) {
-                        Text(stringResource(R.string.Suma))
-                    }
-                }
-            }
-        }
+    // Estado para el resultado de la operación actual
+    var resultadoActual by mutableStateOf("")
+
+    // Lista para almacenar resultados anteriores
+    var resultadosAnteriores = mutableListOf<String>()
+
+    // Función para realizar la suma
+    fun sumar() {
+        val num1 = numero1.toIntOrNull() ?: 0
+        val num2 = numero2.toIntOrNull() ?: 0
+
+        val resultado = num1 + num2
+
+        // Actualizar resultado actual
+        resultadoActual = "La suma es: $resultado"
+
+        // Guardar resultado en la lista de resultados anteriores
+        resultadosAnteriores.add("$num1 + $num2 = $resultado")
+
+        // Limpiar los campos de entrada
+        numero1 = ""
+        numero2 = ""
     }
-
+}
