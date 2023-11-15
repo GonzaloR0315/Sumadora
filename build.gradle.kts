a121b1debf8dcd2f8def89e6852cdd1cf66a753d
Gonzalo <gonzalo.rubio.gutierrez@gmail.com>
2023-11-15 12:03:35 +0100

    .

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,11 +3,4 @@ plugins {
     id("com.android.application") version "8.1.1" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
 
-    id ("com.android.application")
-    id ("kotlin-android")
-    id ("kotlin-android-extensions")
-    id ("androidx.navigation.safeargs.kotlin")
-    id ("org.jetbrains.kotlin.android")
-    // Otros plugins necesarios
-
 }
