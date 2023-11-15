5ab83038fde292badf37826274a8c763ca732097
Gonzalo <gonzalo.rubio.gutierrez@gmail.com>
2023-11-15 11:51:15 +0100

    Cada vez menos pero aun falla

--- app/src/main/java/com/example/sumadora/MainActivity.kt
+++ app/src/main/java/com/example/sumadora/MainActivity.kt
@@ -7,10 +7,13 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.*
 import com.example.sumadora.ui.theme.SumadoraTheme
 
@@ -32,18 +35,15 @@ fun SumadoraApp() {
         startDestination = Screen.Input.route
     ) {
         composable(Screen.Input.route) {
-            InputScreen(
-                navController = navController,
-                viewModel = viewModel,
-                onSumResult = {
-                    navController.navigate(Screen.Result.withArgs(it))
-                }
-            )
+            InputScreen(navController = navController, viewModel = viewModel) { result ->
+                navController.navigate(Screen.Result.withArgs(result))
+            }
         }
         composable(Screen.Result.route) { backStackEntry ->
             val result = requireNotNull(backStackEntry.arguments?.getString(Screen.Result.ARG_RESULT))
-            Resultados(navController, viewModel, result) {
+            Resultados(navController = navController, viewModel = viewModel, result = result) {
                 navController.navigate(Screen.Input.route)
             }
         }
     }
+}
--- app/src/main/java/com/example/sumadora/Resultados.kt
+++ app/src/main/java/com/example/sumadora/Resultados.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
@@ -17,18 +18,30 @@ fun Resultados(navController: NavController, viewModel: SumadoraScreen, result: 
             .background(MaterialTheme.colorScheme.surface)
             .padding(20.dp)
     ) {
-        // Resto del código permanece igual
+        Text(stringResource(R.string.resultado), style = MaterialTheme.typography.labelMedium)
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(result, style = MaterialTheme.typography.bodySmall)
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(stringResource(R.string.resultados), style = MaterialTheme.typography.labelMedium)
+        Spacer(modifier = Modifier.height(8.dp))
+
+        val resultadosAnterioresText = buildString {
+            for (resultado in viewModel.resultadosAnteriores) {
+                append(resultado)
+                append("\n")
+            }
+        }
+
+        Text(resultadosAnterioresText, style = MaterialTheme.typography.bodySmall)
 
         // Botón para volver a la pantalla de entrada
         Button(
-            onClick = {
-                onBack()
-            },
+            onClick = onBack,
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(8.dp)
         ) {
-            Text("Volver a la entrada")
+            Text(stringResource(R.string.volver))
         }
     }
 }
