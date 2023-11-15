759f06f01ff87292ac492aad6e28b2408e8eefaf
Gonzalo <gonzalo.rubio.gutierrez@gmail.com>
2023-11-15 11:59:43 +0100

    Solo queda composable

--- app/src/main/java/com/example/sumadora/MainActivity.kt
+++ app/src/main/java/com/example/sumadora/MainActivity.kt
@@ -1,22 +1,15 @@
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHost
 import com.example.sumadora.InputScreen
 import com.example.sumadora.Resultados
 import com.example.sumadora.Screen
 import com.example.sumadora.SumadoraScreen
-import com.example.sumadora.ui.theme.SumadoraTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
